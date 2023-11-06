42e907610f96697b0c306a88d8beabd6a230d02a
Omar Badr <omarbadr938@gmail.com>
2023-11-06 20:49:07 +0100

    add js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const currentDate = document.querySelector('.current-date');
+
+// getting new date, current year and month
+let data = new Date()
+currYear = date.getFullYear()
+currMonth = date.getMonth()
+
+
+console.log(currentDate);
